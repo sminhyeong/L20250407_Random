a85d8dc1cd6eb87e40d0349f7bd18cb5b7fc65f1
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2025-04-07 10:52:47 +0900

    L20250407_2 알고리즘 적용한 코드로 변경
    알고리즘 적용한 코드로 변경하여 커밋

--- L20250407_Random.cpp
+++ L20250407_Random.cpp
@@ -6,47 +6,78 @@
 
 int main()
 {
-	bool IsComplete = false;
-	int idx = 0;
-	int MaxCount = 10000;
-	int Balls[10000] = { 0, };
+	int MaxCount = 52;
+	int Balls[52] = { 0, };
 
-	srand(time(NULL));
+	for (int i = 0; i < MaxCount; i++)
+	{
+		Balls[i] = (i + 1);
+	}
 
 	time_t StartTime = time(NULL);
+	srand((unsigned int)time(NULL));
 
-	while (!IsComplete)
+	//¼ÅÇÃ
+	int temp = 0;
+	for (int count = 0; count < MaxCount * 1000; count++)
 	{
-		int Num = rand()%MaxCount + 1;
-		bool Duplecate = false;
+		int Idx1 = rand() % MaxCount;
+		int Idx2 = rand() % MaxCount;
 
-		for (int i = 0; i < idx+1; i++)
-		{
-			if (Num == Balls[i])
-			{
-				Duplecate = true;
-				break;
-			}
-		}
-
-		if (!Duplecate)
-		{
-			Balls[idx] = Num;
-			idx++;
-
-			if (idx >= MaxCount)
-			{
-				IsComplete = true;
-			}
-		}
+		temp = Balls[Idx1];
+		Balls[Idx1] = Balls[Idx2];
+		Balls[Idx2] = temp;
 	}
+
 	time_t EndTime = time(NULL);
 	double DebugTime = (double)(EndTime - StartTime);
 	printf("DebugTime : %f\n", DebugTime);
-	/*printf("Boal Number : ");
+
+	//Ãâ·Â
+	printf("Boal Number : ");
 	for (int i : Balls)
 	{
 		printf("%d, ", i);
-	}*/
+	}
 	return 0;
 }
+
+//int main()
+//{
+//	int ValueIdx = 0;
+//	int MaxCount = 52;
+//	int Balls[52] = { 0, };
+//	int ValueBalls[52] = { 0, };
+//
+//	for (int i = 0; i < MaxCount; i++)
+//	{
+//		Balls[i] = (i + 1);
+//	}
+//
+//	time_t StartTime = time(NULL);
+//	srand((unsigned int)time(NULL));
+//
+//	while (MaxCount > 0)
+//	{
+//		int idx = rand() % MaxCount;
+//
+//		ValueBalls[ValueIdx] = Balls[idx];
+//		for (int i = idx; i < MaxCount-1; i++)
+//		{
+//			Balls[i] = Balls[i + 1];
+//		}
+//
+//		ValueIdx++;
+//		MaxCount--;
+//	}
+//
+//	time_t EndTime = time(NULL);
+//	double DebugTime = (double)(EndTime - StartTime);
+//	printf("DebugTime : %f\n", DebugTime);
+//	printf("Boal Number : ");
+//	for (int i : ValueBalls)
+//	{
+//		printf("%d, ", i);
+//	}
+//	return 0;
+//}
